1e11ac94a27bd1ce09363cad5fcf7fc735e01021
BiodunIjelu <biodunijelu@gmail.com>
2024-01-14 14:35:18 +0100

    adding 'scores.js' into js folder

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,9 +1,20 @@
 // Adding Sample questions array
 const quizQuestions = [
     {
-        question: "What does HTML stand for?",
-        choices: ["Hyper Text Markup Language", "Hyperlinks and Text Markup Language", "Home Tool Markup Language", "Hyper Transfer Markup Language"],
+        question: "1. What does HTML stand for?",
+        choices: ["a) Hyper Text Markup Language", "b) Hyperlinks and Text Markup Language", "c) Home Tool Markup Language", "d) Hyper Transfer Markup Language"],
         correctAnswer: "Hyper Text Markup Language"
     },
+
+    {
+        question: "2. What is the capital of France?",
+        choices: ["a) Paris", "b) Berlin", "c) Madrid"],
+        correctAnswer: "a) Paris"
+    },
    
+    {
+        question: "3. What is the largest planet in our solar system?",
+        choices: ["a) Earth", "b) Jupiter", "c) Mars"],
+        correctAnswer: "b) Jupiter"
+    },
 ];

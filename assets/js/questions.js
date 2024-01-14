eb66b96e6016dde72a8e6065d742002b3e04b921
BiodunIjelu <biodunijelu@gmail.com>
2024-01-14 12:51:01 +0100

    Adding Sample questions array into the 'question.js' file

---
+++ assets/js/questions.js
@@ -0,0 +1,9 @@
+// Adding Sample questions array
+const quizQuestions = [
+    {
+        question: "What does HTML stand for?",
+        choices: ["Hyper Text Markup Language", "Hyperlinks and Text Markup Language", "Home Tool Markup Language", "Hyper Transfer Markup Language"],
+        correctAnswer: "Hyper Text Markup Language"
+    },
+   
+];

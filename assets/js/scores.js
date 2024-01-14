3358afebc58a189b8410f6591ee3548272b61de8
BiodunIjelu <biodunijelu@gmail.com>
2024-01-14 16:41:52 +0100

    Retrieve and display the stored final-score and initials

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Retrieve the final score from localStorage
     const finalScore = localStorage.getItem("finalScore");
+    const userInitials = localStorage.getItem("userInitials");
 
     // Display the final score on the highscores page
     document.getElementById("final-score").textContent = finalScore;
-
+    document.getElementById("user-initials").textContent = userInitials;
     
     // For example, I create a new list item and append it to the highscores list
     if (finalScore) {
